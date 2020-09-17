f8bca6f97736ee96f3248a408071523571d9fb36
Grand Zhao <grand.zhao@eventbank.com>
2020-09-17 22:24:00 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,9 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: "airbnb-base",
+  extends: 'airbnb-base',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
   },
   rules: {},
 };
